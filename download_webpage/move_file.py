83464e3f089dd8faf30938ef280c1d89773b14b7
David Branner <brannerchinese@gmail.com>
2014-08-01 17:36:58 -0400

    separate opening and moving functions

--- download_webpage/move_file.py
+++ download_webpage/move_file.py
@@ -1,20 +1,24 @@
 #! /usr/bin/env python
 # move_file.py
 # David Prager Branner
-# 20140719
+# 20140801
 
 """Read file and move out of the way."""
 
 import os
 import shutil
 
-def move(filename):
+def open(filename):
     try:
         with open(filename, 'r') as f:
             content = f.read()
     except FileNotFoundError:
         print('File {} not found.'.format(filename))
         return None
+    return content
+
+def move(filename):
+    content = open(filename)
     if not os.path.exists('saved_downloads'):
         os.mkdir('saved_downloads')
     try:
